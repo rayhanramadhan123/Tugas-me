c37e87638b6fa71a7e3fb7fa31a58893ec9d1ca6
rayhanramadhan123 <113223481+rayhanramadhan123@users.noreply.github.com>
2022-09-10 10:31:17 +0700

    Add files via upload

---
+++ pelatihan cod/rehan.py
@@ -0,0 +1,41 @@
+from unicodedata import name
+
+
+def fungsi_python():
+   print("hai ini saya")
+
+fungsi_python()
+# ini komentar
+
+nama = "Rayhan R"
+umur = "18"
+ukuran_sepatu = "42,5"
+print("Nama saya adalah " + nama + " umur saya adalah " + str(18))
+print("umur saya adalah :", umur)
+print(ukuran_sepatu, "dia adalah", type(ukuran_sepatu))
+
+# list <Array>
+
+binatang = ["kucing", "ayam","anjinng"]
+binatangbuas = ("singa", "macan","anjinng")
+namamhs = {"rayhan", "joko"}
+
+print(binatang[1])
+
+
+# list
+angka1 = 5
+angka2 = 10
+angka3 = 20
+print("penjumlahan :" + str(angka1 + angka2 + angka3))
+
+print("penguranga :" + str(angka1 - angka2 - angka3))
+
+print("pembagian :" + str(angka1 / angka2))
+
+print("pembagian 2 :" + str(63 // 10))
+
+print("perkalian :" + str(angka1 * angka2 * 10))
+
+print("modulus :" + str(100 % 50))
+
