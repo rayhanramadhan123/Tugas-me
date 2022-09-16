dd2122f0504340a091289577a1b4e08bad6925b6
Muhammad Rayhan Ramadhan <113223481+rayhanramadhan123@users.noreply.github.com>
2022-09-16 07:45:46 +0700

    Konversi suhu

---
+++ konversi.py
@@ -0,0 +1,22 @@
+print("koversi celcius ke fahrenheit")
+
+def konversuhu (S) :
+    suhu = s
+    a = suhu
+    suhu = float (9/5) * a + 32
+
+    return suhu
+
+s = int(input())
+print("Hasil dari", "celcius to fahrenheit =",konversuhu(s))
+
+print("koversi kelvin ke celcius")
+
+def konversuhu (X) :
+    suhu = x
+    c = suhu
+    suhu = float (273 * 15) - c
+
+    return suhu
+x = int(input())
+print("Hasil dari", "kelvin to celcius =", konversuhu(x))
